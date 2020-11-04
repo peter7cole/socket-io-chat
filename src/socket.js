b716d438b09e49dbb3c1c343bf1f46239caeb392
‘peter-robert-cole’ <petercole3@gmail.com>
2020-11-04 10:10:42 -0800

    adding socket.io-client

---
+++ src/socket.js
@@ -0,0 +1,14 @@
+import io from 'socket.io-client';
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
